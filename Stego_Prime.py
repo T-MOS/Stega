45a326809cfe157d83c74ed4ae79f71c9edc34d8
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-20 19:11:52 -0500

    Update Stego_Prime.py
    simple text <->bin

--- Stego_Prime.py
+++ Stego_Prime.py
@@ -2,9 +2,20 @@ import sys
 import numpy as np
 from PIL import Image
 
-if len(sys.argv) != 3:
-    sys.exit(1)
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+      sys.exit(1)
 
-def image_accessor(image_path = sys.argv[1],text_file = sys.argv[2]):
+def image_accessor(image_path = sys.argv[1]):
   openInitial = Image.open(image_path)
-  arrayed = np.array(openInitial)
+  image_array = np.array(openInitial)
+  return image_array
+
+def text_to_binary(text_file = sys.argv[2]):
+  with open(text_file, 'r') as t:
+    binary_data = ''.join(format(byte,'08b') for byte in bytearray(t, 'utf-8'))
+  return binary_data
+
+def binary_decode(bytes):
+  string_out = ''.join(chr(int(bytes[i:i+8],2)) for i in range(0,len(bytes),8))
+  return string_out
