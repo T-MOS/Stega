37c02258d11ff4c0829ca303ae67c92b0be189dc
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-22 17:56:46 -0500

    Update Stego_Prime.py
    added padder()

--- Stego_Prime.py
+++ Stego_Prime.py
@@ -22,14 +22,15 @@ def image_accessor(image_path = sys.argv[1]):
 
 def text_to_dec(text_file = sys.argv[2]):
   with open(text_file, 'r') as t:
-    decimalized = [byte for byte in bytearray(t, 'utf-8')]
-    decimalShaped = np.array(decimalized).reshape(-1,3)
+    decimalized = np.array([byte for byte in bytearray(t, 'utf-8')])
+    decimalShaped = pad_and_reshape_text_decimals(decimalized)
   return decimalShaped
 
 def binary_decode(binString):
   # string_out = ''.join(chr(int(bytes[i:i+8],2)) for i in range(0,len(bytes),8))
   toBytes = bytes([int(binString)])
   return toBytes
+
 
 def pad_and_reshape_text_decimals(decimal_array):
   fit = len(decimal_array) % 3
@@ -41,17 +42,22 @@ def pad_and_reshape_text_decimals(decimal_array):
   padded = np.pad(decimal_array,(0,rightPad), mode = "constant").reshape(-1,3)
   return padded
 
-
-def arrayOps(decimal_array):
-  rowsToAdd = len(decimal_array)
-  colsToAdd = image_array.shape[1]
-  addRows = np.zeros(,colsToAdd,3)
+def array_operations(shaped_array):
+  row_count = (len(shaped_array)//len(image_array))+1
+  cols = image_array.shape[1]
+  zerows = np.zeros((row_count,cols,3))
+  
   # flatten,add,unflatten
-  flat_image_array = image_array.reshape(-1,3)
-
-  for i in range(len(flat_stacked)-41,len(flat_stacked)-41+len(decArr)):
-    flat_stacked[i] = decArr[i % len(decArr)]
-
-  embedded = embedded.astype(np.uint8)
-
+  flat_stacked = np.vstack((image_array,zerows)).reshape(-1,3)
+  added_start = len(flat_stacked)-(row_count*cols)
+  insert_stop = added_start+len(shaped_array)
+  for i in range(added_start,insert_stop):
+    flat_stacked[i] = shaped_array[i % len(shaped_array)]
+  rebuilt = flat_stacked.reshape(-1,cols,3).astype(np.uint8)
+  
+  embedded = Image.fromarray(rebuilt)
   return embedded
+
+image_array = image_accessor()
+decimal_shaped = text_to_dec(image_array)
+text_embedded = array_operations(decimal_shaped)
