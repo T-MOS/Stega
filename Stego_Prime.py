2a9a91ded18485bdf6cd7e2e46785a4c2d4d8939
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-22 16:39:10 -0500

    Update Stego_Prime.py

--- Stego_Prime.py
+++ Stego_Prime.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 if __name__ == "__main__":
   if len(sys.argv) != 3:
-      sys.exit(1)
+    sys.exit(1)
 
 def image_accessor(image_path = sys.argv[1]):
   openInitial = Image.open(image_path)
@@ -20,19 +20,25 @@ def image_accessor(image_path = sys.argv[1]):
 #   string_out = ''.join(chr(int(bytes[i:i+8],2)) for i in range(0,len(bytes),8))
 #   return string_out
 
-def text_to_binary(text_file = sys.argv[2]):
+def text_to_dec(text_file = sys.argv[2]):
   with open(text_file, 'r') as t:
     decimalized = [byte for byte in bytearray(t, 'utf-8')]
-  return decimalized
+    decimalShaped = np.array(decimalized).reshape(-1,3)
+  return decimalShaped
 
 def binary_decode(binString):
   # string_out = ''.join(chr(int(bytes[i:i+8],2)) for i in range(0,len(bytes),8))
   toBytes = bytes([int(binString)])
-  return string_out
+  return toBytes
 
-def arrayOps(image_array):
+def pad_reshape(decimal_array):
+  
+
+
+def arrayOps(decimal_array):
+  rowsToAdd = len(decimal_array)
   colsToAdd = image_array.shape[1]
-  addRow = np.zeros(,image_array.shape[1],3)
+  addRows = np.zeros(,colsToAdd,3)
   # flatten,add,unflatten
   flat_image_array = image_array.reshape(-1,3)
 
