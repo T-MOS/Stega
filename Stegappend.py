82f162650b993e90b451b3bcccb6574eab55e8bc
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-23 11:49:34 -0500

    Update Stegappend.py

--- Stegappend.py
+++ Stegappend.py
@@ -3,25 +3,27 @@ import numpy as np
 from PIL import Image
 
 # run conditions
-
-# 1) CLI encode
+# CLI
 if __name__ == "__main__":
-  if len(sys.argv) != 3:
+  # 1) encode "__main__"-> 2 args: image/.txt
+  if len(sys.argv) == 3:
+    image_path = sys.argv[1]
+    text_file = sys.argv[2]
+  # 2) CLI decode "__main__"-> 1 arg: image
+  if len(sys.argv)
+  else:
     sys.exit(1)
-    
-# 2) CLI decode -> arg = image
+# 3) module encode "Stegappend"-> 2 params:
 
-# 3) module encode
+# 4) module decode "Stegappend" -> 1 arg
 
-# 4) module decode
-
-def image_accessor(image_path = sys.argv[1]):
-  openInitial = Image.open(image_path)
+def image_accessor(image = sys.argv[1]):
+  openInitial = Image.open(image)
   image_array = np.array(openInitial)
   return image_array
 
-def text_to_dec(text_file = sys.argv[2]):
-  with open(text_file, 'r') as t:
+def text_to_dec(text = sys.argv[2]):
+  with open(text, 'r') as t:
     text = t.read()
     decimalized = np.array([byte for byte in bytearray(text, 'utf-8')])
     decimalShaped = pad_and_reshape_text_decimals(decimalized)
@@ -47,7 +49,7 @@ def array_operations(shaped_array):
   cols = image_array.shape[1]
   zerows = np.zeros((row_count,cols,3))
   
-  # flatten,add,unflatten
+  # flatten,add,rebuild
   flat_stacked = np.vstack((image_array,zerows)).reshape(-1,3)
   added_start = len(flat_stacked)-(row_count*cols)
   insert_stop = added_start+len(shaped_array)
@@ -55,8 +57,8 @@ def array_operations(shaped_array):
     flat_stacked[i] = shaped_array[i % len(shaped_array)]
   rebuilt = flat_stacked.reshape(-1,cols,3).astype(np.uint8)
   
-  embedded = Image.fromarray(rebuilt)
-  return embedded
+  text_embedded_image = Image.fromarray(rebuilt)
+  return text_embedded_image
 
 
 image_array = image_accessor() # argv 1
