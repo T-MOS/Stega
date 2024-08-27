bc71be27d8f22367871038da6cd05c8ff38fc9d4
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-27 15:19:56 -0500

    Update Stega Prime.py

--- Stega Prime.py
+++ Stega Prime.py
@@ -2,9 +2,9 @@ import sys, os
 import numpy as np
 from PIL import Image
 
-if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    sys.exit(1)
+# if __name__ == "__main__":
+#   if len(sys.argv) != 3:
+#     sys.exit(1)
 
 def image_accessor(image):
   if isinstance(image, Image.Image):
@@ -61,32 +61,22 @@ def binary_decode(binString):
   toBytes = bytes([int(binString)])
   return toBytes
 
-def pad_and_reshape_text_decimals(decimal_array):
-  fit = len(decimal_array) % 3
-  rightPad = 0
+# image_array = image_accessor()
+# decimal_shaped = text_to_dec(image_array)
+# text_embedded = array_operations(decimal_shaped)
 
-  if fit != 0:
-    rightPad = 3-fit
+embed_index = 0
+new_image_pixels = []
+for pixel in i_array_byte:
+  if embed_index < len(initials_binary):
+    new_pixel = []
+    for channel in pixel:
+      if embed_index < len(initials_binary)
+        new_pixel.append((channel & ~1) | int(initials_binary[embed_index]))
+        embed_index += 1
+      else:
+        new_pixel.append(channel)
+      new_image_pixels.append(tuple(new_pixel)) # add full new pixel to new image  
 
-  padded = np.pad(decimal_array,(0,rightPad), mode = "constant").reshape(-1,3)
-  return padded
-
-def array_operations(shaped_array):
-  row_count = (len(shaped_array)//len(image_array))+1
-  cols = image_array.shape[1]
-  zerows = np.zeros((row_count,cols,3))
-  
-  # flatten,add,unflatten
-  flat_stacked = np.vstack((image_array,zerows)).reshape(-1,3)
-  added_start = len(flat_stacked)-(row_count*cols)
-  insert_stop = added_start+len(shaped_array)
-  for i in range(added_start,insert_stop):
-    flat_stacked[i] = shaped_array[i % len(shaped_array)]
-  rebuilt = flat_stacked.reshape(-1,cols,3).astype(np.uint8)
-  
-  embedded = Image.fromarray(rebuilt)
-  return embedded
-
-image_array = image_accessor()
-decimal_shaped = text_to_dec(image_array)
-text_embedded = array_operations(decimal_shaped)
+    else: # no more message, add og pixel vals
+      new_image_pixels.append(pixel)
